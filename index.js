8a5596a00da7a3bd13b52027336e3e069e9ba72c
Abdul-Jalil Khalifah <abduljalilibnismail2@gmail.com>
2026-06-16 20:05:50 +0300

    Download & Import NPM Packages

---
+++ index.js
@@ -0,0 +1,6 @@
+import ejs from "ejs";
+import express from "express";
+import bodyParser from "body-parser";
+import env from "dotenv";
+import hijriDate from "hijri-date";
+import axios from "axios";
