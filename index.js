5eef24fb4961e5bb1bdfc6ee156ce561b3145e81
Abdul-Jalil Khalifah <abduljalilibnismail2@gmail.com>
2026-06-17 18:03:34 +0300

    Add & Test GET and POST Routes

--- index.js
+++ index.js
@@ -1,6 +1,61 @@
-import ejs from "ejs";
-import express from "express";
-import bodyParser from "body-parser";
-import env from "dotenv";
-import hijriDate from "hijri-date";
-import axios from "axios";
+import ejs from 'ejs';
+import express from 'express';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import hijriDate from 'hijri-date';
+import axios from 'axios';
+
+env.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const HijriDate = hijriDate.default || hijriDate;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+const currentYear = new Date().getFullYear();
+
+const currentHijriYear = new HijriDate().getFullYear();
+
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { books: null, error: null, currentYear: currentYear, currentHijriYear: currentHijriYear });
+});
+
+
+app.post('/search', async (req, res) => {
+    const searchType = req.body['search-type'];
+    const searchQuery = req.body['search-query'];
+
+    let apiParams = 'q';
+    if (searchType && searchType != 'general') {
+        apiParams = searchType;
+    }
+
+    try {
+        const result = await axios.get('https://openlibrary.org/search.json', {
+            params: {
+                [apiParams]: searchQuery,
+                limit: 5
+            }
+        });
+
+        const booksData = result.data.docs;
+
+        if (booksData.length === 0) {
+            res.render('index.ejs', { books: null, error: 'No books found matching your criteria.' });
+        }
+
+        res.render('index.ejs', { books: booksData, error: null });
+    } catch (error) {
+        console.error('Api Fetch Error:', error.message);
+        res.render('index.ejs', { books: null, error: 'Failed to retrieve data from Open Library. Please try again.' });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`BookWorm is running on port: ${port}`);
+});
--- public/index.js
+++ public/index.js
@@ -1,34 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const dropdownToggle = document.querySelector(".dropdown-toggle");
-  const dropdownMenu = document.querySelector(".search-parameters");
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdownToggle = document.querySelector('.dropdown-toggle');
+  const dropdownMenu = document.querySelector('.search-parameters');
 
-  dropdownToggle.addEventListener("click", (e) => {
+  dropdownToggle.addEventListener('click', (e) => {
     e.stopPropagation();
-    const isExpanded = dropdownToggle.getAttribute("aria-expanded") === "true";
+    const isExpanded = dropdownToggle.getAttribute('aria-expanded') === 'true';
 
-    dropdownMenu.classList.toggle("show");
-    dropdownToggle.setAttribute("aria-expanded", !isExpanded);
+    dropdownMenu.classList.toggle('show');
+    dropdownToggle.setAttribute('aria-expanded', !isExpanded);
   });
 
-  document.addEventListener("click", (e) => {
+  document.addEventListener('click', (e) => {
     if (!dropdownMenu.contains(e.target) && e.target !== dropdownToggle) {
-      dropdownMenu.classList.remove("show");
-      dropdownToggle.setAttribute("aria-expanded", "false");
+      dropdownMenu.classList.remove('show');
+      dropdownToggle.setAttribute('aria-expanded', 'false');
     }
   });
 });
 
-const themeButton = document.querySelector("#theme-btn");
-const cssLink = document.querySelector("#css-link");
+const themeButton = document.querySelector('#theme-btn');
+const cssLink = document.querySelector('#css-link');
 
-themeButton.addEventListener("click", () => {
-  if (cssLink.getAttribute("href") === "css/main.css") {
-    cssLink.setAttribute("href", "css/darkmode.css");
+themeButton.addEventListener('click', () => {
+  if (cssLink.getAttribute('href') === '/css/main.css') {
+    cssLink.setAttribute('href', '/css/darkmode.css');
 
-    themeButton.classList.add("active");
+    themeButton.classList.add('active');
   } else {
-    cssLink.setAttribute("href", "css/main.css");
+    cssLink.setAttribute('href', '/css/main.css');
 
-    themeButton.classList.remove("active");
+    themeButton.classList.remove('active');
   }
 });
